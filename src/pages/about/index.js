eb71fddb3e43921495f708f17aff47bc2f7813a5
Richard Westmoreland <kegtrack@gmail.com>
2020-02-04 13:11:19 -0600

    move global css to borwser file

--- src/pages/about/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Layout from '../../components/layout'
-import SEO from '../../components/seo'
-
-const AboutPage = () => {
-  return <div></div>
-}
-
-export default AboutPage
--- src/pages/beer/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Layout from '../../components/layout'
-import SEO from '../../components/seo'
-
-const BeerPage = () => {
-  return <div></div>
-}
-
-export default BeerPage
